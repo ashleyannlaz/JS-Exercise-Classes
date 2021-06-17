a11f245b4db73d0657258fef7deb66013de5e81b
ashleyannlaz <paperpastels@gmail.com>
2021-06-17 19:14:03 -0400

    Finished Task2

--- index.js
+++ index.js
@@ -117,10 +117,13 @@ console.log()
   */
  class Lambdasian {
    constructor(obj){
+      this.name = obj.name;
+      this.age = obj.age;
+      this.location = obj.location;
 
    }
    speak(){
-
+      return `Hello my name is ${this.name}, I am from ${this.location}`
    }
   }
   
@@ -138,8 +141,19 @@ console.log()
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
-
+ class Instructor extends Lambdasian {
+    constructor(obj){
+      super(obj);
+      this.specialty = obj.specialty;
+      this.favLanguage = obj.favLanguage;
+      this.catchPhrase = obj.catchPhrase;
+    }
+      demo(subject){
+        return `Today we are learning about ${this.subject}.`
+      }
+      grade(student){
+        return `${this.name} receives a perfect score on ${this.subject}`;
+      }
  }
   /*
     TASK 5
@@ -156,7 +170,7 @@ console.log()
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
+ class Student extends Lambdasian{
      
  }
   
